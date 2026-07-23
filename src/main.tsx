fef1f4a9f0ddb15a213b11fecb2b25510448e383
playon7 <sallesgbl@gmail.com>
2026-07-23 14:37:32 -0400

    last fix i think

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter basename="/playon-website">
+    <BrowserRouter basename="/playon-website/">
       <App />
     </BrowserRouter>
   </StrictMode>,
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/playon-website",
+  base: "/playon-website/",
   plugins: [react()],
 })
